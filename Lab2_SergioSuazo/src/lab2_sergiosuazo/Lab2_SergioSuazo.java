4e4d64b14dceebe393cff746bbe587ea6d36ed57
Sergio Suazo <sergiosb1001@gmail.com>
2020-01-31 13:23:43 -0600

    inicio

---
+++ Lab2_SergioSuazo/src/lab2_sergiosuazo/Lab2_SergioSuazo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_sergiosuazo;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Lab2_SergioSuazo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
